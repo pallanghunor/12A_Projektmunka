8ca0fa5fff6427fa124fb933473ad104d766e6c1
pallanghunor <pallang.hunor@students.jedlik.eu>
2023-11-08 22:31:44 +0100

    nevek atirasa

--- 12A_Projektmunka/Weapon.cs
+++ 12A_Projektmunka/Weapon.cs
@@ -9,7 +9,7 @@ namespace _12A_Projektmunka
     class Weapon
     {
         public string Name { get; set; }
-        public string Type { get; set; }
+        public string WeaponType { get; set; }
         public string FileName { get; set; }
         public int Cost { get; set; }
         public int Ammo { get; set; }
@@ -22,7 +22,7 @@ namespace _12A_Projektmunka
         {
             string[] data = row.Split(';');
             Name = data[0];
-            Type = data[1];
+            WeaponType = data[1];
             FileName = data[2];
             Cost = int.Parse(data[3]);
             Ammo = int.Parse(data[4]);
